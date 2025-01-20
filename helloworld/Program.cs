dd984c2a6190e50d1b779192aeb8662f93742c11
Akshath Sai, Pittala <akshathsai.pittala@outlook.com>
2025-01-20 23:00:09 +0530

    Playing with String methods

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -2,11 +2,14 @@
 using System;
 namespace ConsoleApplication
 {
-    class HelloWorld {
+    class HelloWorld
+    {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
             GetSubString.test();
+            JoiningStrings joiningStrings = new JoiningStrings();
+            joiningStrings.test();
         }
     }
 
@@ -19,7 +22,20 @@ namespace ConsoleApplication
             string substr = str.Substring(23);
             Console.WriteLine(substr);
         }
+    }
 
+    class JoiningStrings
+    {
+        public void test()
+        {
+            string[] starray = new string[]{
+            "Down the way nights are dark", 
+            "And the sun shines daily on the mountain top",
+            "I took a trip on a sailing ship",
+            "And when I reached Jamaica",
+            "I made a stop"};
+            string str = String.Join("\n", starray); Console.WriteLine(str);
+        }
     }
 
 }
