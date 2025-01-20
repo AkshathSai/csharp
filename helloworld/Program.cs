bd1e1077882692778dfee03097e8d8b8627701ec
Akshath Sai, Pittala <akshathsai.pittala@outlook.com>
2025-01-20 22:56:01 +0530

    Playing with string methods

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+namespace ConsoleApplication
+{
+    class HelloWorld {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            GetSubString.test();
+        }
+    }
+
+    class GetSubString //Getting a Substring
+    {
+        public static void test()
+        {
+            string str = "Last night I dreamt of San Pedro";
+            Console.WriteLine(str);
+            string substr = str.Substring(23);
+            Console.WriteLine(substr);
+        }
+
+    }
+
+}
